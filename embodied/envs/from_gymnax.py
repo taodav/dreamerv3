0a0424025b14b732ced553dae22843e02a0f0cf0
Ruo Yu Tao <ruoyutao@gmail.com>
2024-07-17 14:35:19 -0400

    bump

--- embodied/envs/from_gymnax.py
+++ embodied/envs/from_gymnax.py
@@ -1,5 +1,7 @@
+import functools
 
 import chex
+
 import embodied
 
 
@@ -10,11 +12,57 @@ def get_gymnax_env(env_name: str,
 
 class FromGymnax(embodied.Env):
 
-    def __init__(self, env, obs_key='image', act_key='action', **kwargs):
-        if isinstance(env, str):
-            self._env = get_gymnax_env(env, **kwargs)
-        else:
-            assert not kwargs, kwargs
-            self._env = env
+  def __init__(self, env, env_params, obs_key='image', act_key='action', **kwargs):
+    if isinstance(env, str):
+      self._env = get_gymnax_env(env, **kwargs)
+    else:
+      assert not kwargs, kwargs
+      self._env = env
+    self._state = None
+    # TODO: spaces here?
+    self._obs_dict = hasattr(self._env.observation_space, 'spaces')
+    self._act_dict = hasattr(self._env.action_space, 'spaces')
+    self._obs_key = obs_key
+    self._act_key = act_key
+    self._done = True
+    self._info = None
 
+  @functools.cached_property
+  def obs_space(self):
+    if self._obs_dict:
+      spaces = self._flatten(self._env.observation_space.spaces)
+    else:
+      spaces = {self._obs_key: self._env.observation_space}
+    spaces = {k: self._convert(v) for k, v in spaces.items()}
+    return {
+        **spaces,
+        'reward': embodied.Space(np.float32),
+        'is_first': embodied.Space(bool),
+        'is_last': embodied.Space(bool),
+        'is_terminal': embodied.Space(bool),
+    }
 
+  @functools.cached_property
+  def act_space(self):
+    if self._act_dict:
+      spaces = self._flatten(self._env.action_space.spaces)
+    else:
+      spaces = {self._act_key: self._env.action_space}
+    spaces = {k: self._convert(v) for k, v in spaces.items()}
+    spaces['reset'] = embodied.Space(bool)
+    return spaces
+
+  def step(self, action):
+    if action['reset'] or self._done:
+      self._done = False
+      obs = self._env.reset()
+      return self._obs(obs, 0.0, is_first=True)
+    if self._act_dict:
+      action = self._unflatten(action)
+    else:
+      action = action[self._act_key]
+    obs, reward, self._done, self._info = self._env.step(action)
+    return self._obs(
+      obs, reward,
+      is_last=bool(self._done),
+      is_terminal=bool(self._info.get('is_terminal', self._done)))
